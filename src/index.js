d60550fa7aaac602e16eda22d04be8217618dfb8
Your Name <mohdaalam1993@gmail.com>
2025-03-28 00:35:51 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -9,6 +9,14 @@ dotenv.config({
 // console.log("MongoDB URI:", process.env.MONGODB_URI); 
 
 connectDB() 
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongo db connection failed !!!",err)
+})
 
 
 
